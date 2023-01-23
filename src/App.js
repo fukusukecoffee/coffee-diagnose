2c99c73a209441631a8bb67b2a6f6bdc8d945652
fukusukecoffee <fukusuke.coffee@gmail.com>
2023-01-23 19:31:57 +0900

    fix app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import {Title} from './ui-components'
+import ComicLightlargeBase from './ui-components/ComicLightlargeBase';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://fukusukecoffee.com/wp-content/uploads/2023/01/pink-cup.png" alt="coffee-cup" />
         <Title />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ComicLightlargeBase><p>hello</p></ComicLightlargeBase>
       </header>
     </div>
   );
