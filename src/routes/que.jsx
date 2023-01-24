bbe7e7ac1ce4a1c8b45a561f026351d4bd486ce3
fukusukecoffee <fukusuke.coffee@gmail.com>
2023-01-24 13:43:22 +0900

    add datastore

--- src/routes/que.jsx
+++ src/routes/que.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../App.css";
 
-import {ClickButton, Title} from '../ui-components'
+import {Back, NextButton, Qdata, StartButton, Title} from '../ui-components'
 import ComicLightlargeBase from '../ui-components/ComicLightlargeBase';
 import { Container, NavbarBrand, Navbar, Button } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+// import { DataStore } from 'aws-amplify';
+// import { Questionare } from '../models';
+
 
 export default function que() {
     return (
@@ -16,9 +20,12 @@ export default function que() {
         </NavbarBrand>
       </Navbar>
       <div className="App-header">
-
+        <Container>
+            <img src="https://fukusukecoffee.com/wp-content/uploads/2023/01/social-media-card-frame-1.png" alt="tree" />
+        </Container>
       </div>
     </div>
+    <Back />
       </>
     );
   }
--- src/ui-components/Qdata.jsx
+++ src/ui-components/Qdata.jsx
@@ -41,7 +41,7 @@ export default function Qdata(props) {
         position="relative"
         padding="0px 0px 0px 0px"
         whiteSpace="pre-wrap"
-        children={question?.question}
+        children={question?.asking}
         {...getOverrideProps(
           overrides,
           "\u8CEA\u554F\uFF1A\u30B9\u30DA\u30B7\u30E3\u30EB\u30C6\u30A3\u30B3\u30FC\u30D2\u30FC\u3092\u77E5\u3063\u3066\u3044\u307E\u3059\u304B\uFF1F"
