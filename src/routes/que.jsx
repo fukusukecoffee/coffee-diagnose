67ba95d8f0d93212ba1876d1f0d87188b2b6ed09
fukusukecoffee <fukusuke.coffee@gmail.com>
2023-01-25 23:00:25 +0900

    fix content

--- src/routes/que.jsx
+++ src/routes/que.jsx
@@ -15,50 +15,60 @@ export default function que() {
           <Container><img src="https://fukusukecoffee.com/wp-content/uploads/2022/08/fukusukecoffee-11-1.png" height="15"></img></Container>
         </NavbarBrand>
       </Navbar>
+      <Container className="justify-content-center">
       <div class="web-header">
-        <div className="p-4">お名前</div>
-        <input class="form-control" type="text" placeholder="Default input" aria-label="default input example"></input>
-        <div className="p-4"><Question></Question></div>
-      <Container className="p-4">
-        <div class="row justify-content-center" >
-          <div class="col-3">
-            <Button className="btn-light btn-outline-dark">男性</Button>
+        <div className="p-4">お名前(ニックネーム)</div>
+        <input class="form-control" type="text" placeholder="" aria-label="default input example"></input>
+        <div className="p-4">性別</div>
+        <Container className="p-4">
+          <div class="row justify-content-center" >
+            <div class="col-3">
+              <Button className="btn-light btn-outline-dark">男性</Button>
+            </div>
+            <div class="col-3">
+              <Button className="btn-light btn-outline-dark">女性</Button>
+            </div>
+            <div class="col-3">
+              <Button className="btn-light btn-outline-dark">その他</Button>
+            </div>
           </div>
-          <div class="col-3">
-            <Button className="btn-light btn-outline-dark">女性</Button>
+        </Container>
+        <div className="p-4">年代</div>
+        <div class="row justify-content-center p-4" >
+            <div class="col-3">
+              <Button className="btn-light btn-outline-dark">10代</Button>
+            </div>
+            <div class="col-3">
+              <Button className="btn-light btn-outline-dark">20代</Button>
+            </div>
+            <div class="col-3">
+              <Button className="btn-light btn-outline-dark">30代</Button>
+            </div>
           </div>
-          <div class="col-3">
-            <Button className="btn-light btn-outline-dark">その他</Button>
+          <div class="row justify-content-center p-4" >
+            <div class="col-3">
+              <Button className="btn-light btn-outline-dark">40代</Button>
+            </div>
+            <div class="col-3">
+              <Button className="btn-light btn-outline-dark">50代</Button>
+            </div>
+            <div class="col-3">
+              <Button className="btn-light btn-outline-dark">60代</Button>
+            </div>
           </div>
-        </div>
+          <div class="row justify-content-center p-4" >
+            <div class="col-3">
+              <Button className="btn-light btn-outline-dark">70代</Button>
+            </div>
+            <div class="col-4">
+              <Button className="btn-light btn-outline-dark">80代以上</Button>
+            </div>
+          </div>
+          <div class="p-4"></div>
+        <center><div className="d-grid gap-2 col-4 mx-4"><Button className="btn-dark btn-lg">次へ</Button></div></center>
+      </div>
       </Container>
-      <div className="p-4"><Generation></Generation></div>
-      <div class="row justify-content-center" >
-          <div class="col-3">
-            <Button className="btn-light btn-outline-dark">男性</Button>
-          </div>
-          <div class="col-3">
-            <Button className="btn-light btn-outline-dark">女性</Button>
-          </div>
-          <div class="col-3">
-            <Button className="btn-light btn-outline-dark">その他</Button>
-          </div>
-        </div>
-        <div class="row justify-content-center" >
-          <div class="col-3">
-            <Button className="btn-light btn-outline-dark">男性</Button>
-          </div>
-          <div class="col-3">
-            <Button className="btn-light btn-outline-dark">女性</Button>
-          </div>
-          <div class="col-3">
-            <Button className="btn-light btn-outline-dark">その他</Button>
-          </div>
-        </div>
-      <center><div className="d-grid gap-2 col-4 mx-4"><Button className="btn-dark btn-lg">次へ</Button></div></center>
-
-      </div>
-      </div>
+    </div>
       </>
     );
   }
