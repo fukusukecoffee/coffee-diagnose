7b2af5756e16db43fa68031f53657f3cd9c03218
fukusukecoffee <fukusuke.coffee@gmail.com>
2023-02-08 16:44:13 +0900

    fix result page

--- src/routes/result.jsx
+++ src/routes/result.jsx
@@ -12,9 +12,9 @@ import { Link, useLocation } from "react-router-dom";
 
 
   export default function Result() {
-    const locationSecond = useLocation();
-    const result = locationSecond.state.state
-    console.log(result)
+    // const locationSecond = useLocation();
+    // const result = locationSecond.state.state
+    // console.log(result)
     return (
     <>
   <div className="App">
@@ -22,13 +22,20 @@ import { Link, useLocation } from "react-router-dom";
     <Container className="justify-content-center">
     <div class="web-header">
       <div className="p-4">さくらさんにぴったりなコーヒー豆は・・・</div>
-      <Button className="btn-light btn-outline-dark p-3 disabled">FUKUSUKE BLEND</Button>
-      <div class="p-2"></div>
       <center><img src="https://fukusukecoffee.com/wp-content/uploads/2022/12/2-1.png" alt="fukusukeblend" width="400px" /></center>
-      <div class="p-2"></div>
-      <center><div className="d-grid gap-2"><Link to={`/coffee-q1`}><Button className="btn-dark btn-lg">購入する</Button></Link></div></center>
-      <Link to={`/`}><Back/></Link>
-    <div class="p-4"></div>
+      <div className="p-2"></div>
+      <Container fluid className="lh-1">
+      <p>定番ブレンド</p>
+      <h4>『FUKUSUKE BLEND』</h4>
+      <div className="p-2"></div>
+      <p>ナッツやチョコレートのような風味に</p><p>ハーブのような清涼感。</p>
+      <p>冷めると柑橘のような果実の風味を感じます。</p>
+      Taste note : Chocolate, Herbal, Orange
+      </Container>
+      <div className="p-2"></div>
+      <center><div className="d-grid gap-2"><a href ="https://fukusukecoffee.com/product/fukusuke-blend"><Button className="btn-dark btn-lg">くわしく見る</Button></a></div></center>
+      <Container className="justify-content-center"><img src="https://fukusukecoffee.com/wp-content/uploads/2023/01/pink-cup.png" height="100"></img></Container>
+      <b>＼あなたにおすすめのセットはこちら／</b>
     </div>
     </Container>
   </div>
